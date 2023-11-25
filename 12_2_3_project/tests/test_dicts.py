c3a1b2eb04f543715d48983013f15c1fca6489d7
Yulia Byckova <byckovaulia669@gmail.com>
2023-11-25 20:10:19 +0300

    Change tests

--- 12_2_3_project/tests/test_dicts.py
+++ 12_2_3_project/tests/test_dicts.py
@@ -6,7 +6,7 @@ data = {"vcs": "mercurial"}
 def test_get_val():
     assert dicts.get_val(data, "vcs") == 'mercurial'
     assert dicts.get_val(data, "vcs", "git") == 'mercurial'
-    assert dicts.get_val({}, "vcs", "git") == 'git'
     assert dicts.get_val({}, "vcs", "bazaar") == 'bazaar'
+    assert dicts.get_val({}, "vcs", "git") == 'git'
 
 
